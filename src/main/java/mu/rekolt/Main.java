37fb92f110e2857a52cfcaace013ab6de64048be
Halimatu Sadia Mohammed <h.mohammed@alustudent.com>
2026-08-19 13:23:39 +0400

    Made variable declarations for delivery and payment information

--- src/main/java/mu/rekolt/Main.java
+++ src/main/java/mu/rekolt/Main.java
@@ -2,6 +2,18 @@ package mu.rekolt;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Building Rekolt Produce Tracker");
+
+        // The information for the delivery
+        String MemberID = "M-OO42";
+        String ProduceCode = "BNS";
+        double mass = 236.0;
+        int QualityScore = 91;
+
+        // This part contains information about the payment
+        double BasePrice = 90.0;
+        double GradeMultiplier = 1.15;
+        double CategoryMultiplier = 1.00;
+        double RateOfCommission = 0.05;
+        double TransportLevyPerKg = 2.0;
     }
 }
