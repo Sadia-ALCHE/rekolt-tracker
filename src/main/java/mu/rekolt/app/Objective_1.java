82e0460ee59af2946badea0488053fbfc1388b36
Halimatu Sadia Mohammed <h.mohammed@alustudent.com>
2026-08-24 10:24:43 +0400

    feat: Provided input for delivery processing

---
+++ src/main/java/mu/rekolt/app/Objective_1.java
@@ -0,0 +1,12 @@
+package mu.rekolt.app;
+
+public class Objective_1 {
+    public static void main(String[] args) {
+
+        //Stating the inputs/information needed for delivery
+        String MemberID = "M-OO42"; //This is the member's identity number
+        String ProduceCode = "BNS"; // This is the code assigned to our produce. Eg. 'BNS' is for Beans.
+        double mass = 236.0;    // This is the kg weight for our produce
+        int QualityScore = 91;   // Whole numbered quality score ranked from (0-100)
+    }
+}
