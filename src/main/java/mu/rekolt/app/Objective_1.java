8c75e274bbd472f53cc669878d9c17ab630cd76e
Halimatu Sadia Mohammed <h.mohammed@alustudent.com>
2026-08-24 11:17:04 +0400

    feat: Implemented calculation functions

--- src/main/java/mu/rekolt/app/Objective_1.java
+++ src/main/java/mu/rekolt/app/Objective_1.java
@@ -15,5 +15,25 @@ public class Objective_1 {
         double CategoryMultiplier = 1.00;
         double RateOfCommission = 0.05;
         double TransportLevyPerKg = 2.0;
+
+        // This will help us to calculate the base value
+        double BaseValue = mass * BasePrice;
+
+        // Here's where we will be applying the grade multiplier
+        double GradeValue = BaseValue * GradeMultiplier;
+
+        // Here's where we will be applying the category multiplier
+        double CategoryValue = GradeValue * CategoryMultiplier;
+
+        // This will help us to calculate our commission
+        double Commission = CategoryValue * RateOfCommission;
+
+        // This will help us to calculate the transport levy
+        double TransportLevy = mass * TransportLevyPerKg;
+
+        // This will help us calculate our net payable
+        double NetPayable = CategoryValue - Commission - TransportLevy;
+
+
     }
 }
