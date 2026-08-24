b63058c56a351ffbf37ea6277e81283c0dcb443e
Halimatu Sadia Mohammed <h.mohammed@alustudent.com>
2026-08-24 10:29:55 +0400

    feat: Provided info needed for payment processing

--- src/main/java/mu/rekolt/app/Objective_1.java
+++ src/main/java/mu/rekolt/app/Objective_1.java
@@ -8,5 +8,12 @@ public class Objective_1 {
         String ProduceCode = "BNS"; // This is the code assigned to our produce. Eg. 'BNS' is for Beans.
         double mass = 236.0;    // This is the kg weight for our produce
         int QualityScore = 91;   // Whole numbered quality score ranked from (0-100)
+
+        // This part contains information about the payment
+        double BasePriceForBeans = 90.0;
+        double GradeMultiplier = 1.15;
+        double CategoryMultiplier = 1.00;
+        double RateOfCommission = 0.05;
+        double TransportLevyPerKg = 2.0;
     }
 }
