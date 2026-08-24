1f0114f073161534fbb94881d4ef2cc6f621fd82
Halimatu Sadia Mohammed <h.mohammed@alustudent.com>
2026-08-24 11:25:38 +0400

    feat: Implemented printing function

--- src/main/java/mu/rekolt/app/Objective_1.java
+++ src/main/java/mu/rekolt/app/Objective_1.java
@@ -34,6 +34,21 @@ public class Objective_1 {
         // This will help us calculate our net payable
         double NetPayable = CategoryValue - Commission - TransportLevy;
 
+        // Printing Results
+        System.out.println("REKOLT Planters’ Cooperative Produce Tracker");
+        System.out.println("--------------------------------------------");
 
+        System.out.printf("Member: %s%n", MemberID);
+        System.out.printf("Produce: %s%n", ProduceCode);
+        System.out.printf("Mass: %.2f kg%n", mass);
+        System.out.printf("Quality Score: %d%n", QualityScore);
+
+        System.out.printf("%nBase Value: %.2f MUR%n", BaseValue);
+        System.out.printf("Grade Value: %.2f MUR%n", GradeValue);
+        System.out.printf("Category Value: %.2f MUR%n", CategoryValue);
+        System.out.printf("Commission: %.2f MUR%n", Commission);
+        System.out.printf("Transport Levy: .%.2f MUR%n", TransportLevy);
+
+        System.out.printf("%nNet Payable: %.2f MUR%n", NetPayable);
     }
 }
